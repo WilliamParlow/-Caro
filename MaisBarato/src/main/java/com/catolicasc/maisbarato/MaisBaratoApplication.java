047d2855d9407d7d172e8b351123457c314109ea
WilliamParlow <william.nichelatti@catolicasc.org.br>
2018-06-07 20:09:30 -0300

    Created new Spring Boot project

---
+++ MaisBarato/src/main/java/com/catolicasc/maisbarato/MaisBaratoApplication.java
@@ -0,0 +1,12 @@
+package com.catolicasc.maisbarato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisBaratoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisBaratoApplication.class, args);
+	}
+}
---
+++ MaisBarato/src/test/java/com/catolicasc/maisbarato/MaisBaratoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.catolicasc.maisbarato;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MaisBaratoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
