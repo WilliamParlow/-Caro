3d9f9f40d0cc547f26f04d68ce27a57324c1ec8e
andersonmerten <anderson.merten@gmail.com>
2018-06-07 19:43:25 -0300

    update

---
+++ src/main/java/com/br/catolicasc/springboot/CaroApplication.java
@@ -0,0 +1,12 @@
+package com.br.catolicasc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaroApplication.class, args);
+	}
+}
