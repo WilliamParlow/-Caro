e41dff21a2ee587c7471c61fa6759aaac5999ef8
andersonmerten <anderson.merten@gmail.com>
2018-06-18 17:26:06 -0300

    add CRUD produto
    add view/controller/service for "produto"

---
+++ MuitoMaisBarato/src/main/java/com/br/catolicasc/muitomaisbarato/service/ProdutoService.java
@@ -0,0 +1,38 @@
+package com.br.catolicasc.muitomaisbarato.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.catolicasc.muitomaisbarato.models.Cliente;
+import com.br.catolicasc.muitomaisbarato.models.Produto;
+import com.br.catolicasc.muitomaisbarato.repository.ClienteRepository;
+import com.br.catolicasc.muitomaisbarato.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+	@Autowired
+    private ProdutoRepository repository;
+     
+    public List<Produto> findAll() {
+        return repository.findAll();
+    }
+     
+    public Produto findOne(Long id) {
+        return repository.getOne(id);
+    }
+     
+    public Produto save(Produto produto) {
+        return repository.saveAndFlush(produto);
+    }
+     
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public int contEncomenda() {
+    	return repository.findAll().size();
+    }
+
+}
